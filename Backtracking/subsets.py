644626ed32d1f302d93aa845d2fadf5eae77574a
SPraveeen <praveenji8498@gmail.com>
2025-06-16 23:11:29 +0530

    trees

--- Backtracking/subsets.py
+++ Backtracking/subsets.py
@@ -4,3 +4,15 @@ class Solution:
         for i in nums:
             output += [lst+[i] for lst in output]
         return output
+    
+
+        
+        # [1]
+        # [[],[1]]
+        # 2**n - n -> no.of elements in the list
+
+        # [1,2]
+        # [[],[1],[2],[1,2]]
+
+        # [1,2,3]
+        # [[],[1],[2],[1,2],[3],[]]
